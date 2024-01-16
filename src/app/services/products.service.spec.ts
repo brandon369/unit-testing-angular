a9fad6b6a45d1f4e4c22c3516033ebdb4c7bb241
Brandon A <byarrieta@protonmail.com>
2024-01-15 20:55:38 -0500

    tests: error http

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -4,6 +4,7 @@ import {HttpClientTestingModule, HttpTestingController} from "@angular/common/ht
 import {CreateProductDTO, Product, UpdateProductDTO} from "../models/product.model";
 import {environment} from "../../environments/environment";
 import {generateManyProducts, generateOneProduct} from "../models/product.mock";
+import {HttpStatusCode} from "@angular/common/http";
 
 describe('ProductService', () => {
   let productService: ProductService
@@ -184,7 +185,7 @@ describe('ProductService', () => {
 
     it('sould delete a  product', (doneFn) => {
       const mockData = true
-      const productId = 1
+      const productId = '1'
 
 
       productService.delete(productId).subscribe(data => {
@@ -198,6 +199,96 @@ describe('ProductService', () => {
       expect(req.request.method).toEqual('DELETE')
 
     })
+  })
+
+
+  describe('test getOne', () => {
+
+    it('should return a product', (doneFn) => {
+      // Arrange
+      const mockData: Product = generateOneProduct();
+      const id = '1';
+      // Act
+      productService.getOne(id).subscribe((data) => {
+        // Assert
+        expect(data).toEqual(mockData);
+        doneFn();
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products/${id}`;
+      const req = httpControler.expectOne(url);
+      req.flush(mockData);
+      expect(req.request.method).toEqual('GET');
+    });
+
+    it('should return the right msg when the status code is 404', (doneFn) => {
+      // Arrange
+      const id = '1';
+      const msgError = '404 message';
+      const mockError = {
+        status: HttpStatusCode.NotFound,
+        statusText: msgError,
+      };
+      // Act
+      productService.getOne(id).subscribe({
+        error: (error) => {
+          // assert
+          expect(error).toEqual('El producto no existe');
+          doneFn();
+        },
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products/${id}`;
+      const req = httpControler.expectOne(url);
+      req.flush(msgError, mockError);
+      expect(req.request.method).toEqual('GET');
+    });
+
+    it('should return the right msg when the status code is 409', (doneFn) => {
+      // Arrange
+      const id = '1';
+      const msgError = '409 message';
+      const mockError = {
+        status: HttpStatusCode.Conflict,
+        statusText: msgError,
+      };
+      // Act
+      productService.getOne(id).subscribe({
+        error: (error) => {
+          // assert
+          expect(error).toEqual('Algo esta fallando en el server');
+          doneFn();
+        },
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products/${id}`;
+      const req = httpControler.expectOne(url);
+      req.flush(msgError, mockError);
+      expect(req.request.method).toEqual('GET');
+    });
+
+    it('should return the right msg when the status code is 401', (doneFn) => {
+      // Arrange
+      const id = '1';
+      const msgError = '409 message';
+      const mockError = {
+        status: HttpStatusCode.Unauthorized,
+        statusText: msgError,
+      };
+      // Act
+      productService.getOne(id).subscribe({
+        error: (error) => {
+          // assert
+          expect(error).toEqual('No estas permitido');
+          doneFn();
+        },
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products/${id}`;
+      const req = httpControler.expectOne(url);
+      req.flush(msgError, mockError);
+      expect(req.request.method).toEqual('GET');
+    });
   })
 
 });
